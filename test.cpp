541cffb0bbe6ba2b13537c8a6b1732090d56dc15
Sergey Klevtsov <s.klevzoff@gmail.com>
2021-03-04 16:09:24 -0800

    Add namespaces to test

--- test.cpp
+++ test.cpp
@@ -29,39 +29,48 @@ void test_get_strings(ARGS && ... args)
 
 ///////////////////////////////
 
-enum WeakEnum { A, B };
-ENUM_STRINGS(WeakEnum, "wa", "wb");
-
-enum class StrongEnum : int16_t { A, B };
-ENUM_STRINGS(StrongEnum, "sa", "sb");
-
-struct Foo
+namespace N1
 {
-  enum class NestedEnum { A, B };
-};
-ENUM_STRINGS(Foo::NestedEnum, "fa", "fb");
+  enum WeakEnum { A, B };
+  ENUM_STRINGS(WeakEnum, "wa", "wb");
+}
+
+namespace N2
+{
+  enum class StrongEnum : int16_t { A, B };
+  ENUM_STRINGS(StrongEnum, "sa", "sb");
+}
+
+namespace N3
+{
+  struct Foo
+  {
+    enum class NestedEnum { A, B };
+  };
+  ENUM_STRINGS(Foo::NestedEnum, "fa", "fb");
+}
 
 ///////////////////////////////
 
 int main()
 {
-  test_to_from_string(A, "wa");
-  test_to_from_string(B, "wb");
-  test_stream_io(A);
-  test_stream_io(B);
-  test_get_strings<WeakEnum>("wa", "wb");
+  test_to_from_string(N1::A, "wa");
+  test_to_from_string(N1::B, "wb");
+  test_stream_io(N1::A);
+  test_stream_io(N1::B);
+  test_get_strings<N1::WeakEnum>("wa", "wb");
 
-  test_to_from_string(StrongEnum::A, "sa");
-  test_to_from_string(StrongEnum::B, "sb");
-  test_stream_io(StrongEnum::A);
-  test_stream_io(StrongEnum::B);
-  test_get_strings<StrongEnum>("sa", "sb");
+  test_to_from_string(N2::StrongEnum::A, "sa");
+  test_to_from_string(N2::StrongEnum::B, "sb");
+  test_stream_io(N2::StrongEnum::A);
+  test_stream_io(N2::StrongEnum::B);
+  test_get_strings<N2::StrongEnum>("sa", "sb");
 
-  test_to_from_string(Foo::NestedEnum::A, "fa");
-  test_to_from_string(Foo::NestedEnum::B, "fb");
-  test_stream_io(Foo::NestedEnum::A);
-  test_stream_io(Foo::NestedEnum::B);
-  test_get_strings<Foo::NestedEnum>("fa", "fb");
+  test_to_from_string(N3::Foo::NestedEnum::A, "fa");
+  test_to_from_string(N3::Foo::NestedEnum::B, "fb");
+  test_stream_io(N3::Foo::NestedEnum::A);
+  test_stream_io(N3::Foo::NestedEnum::B);
+  test_get_strings<N3::Foo::NestedEnum>("fa", "fb");
 
   return 0;
 }
